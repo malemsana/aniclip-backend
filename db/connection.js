751a8ec3fac6cc70c1bd87d3342bd8a500e7d15a
Malto <117826083+malemsana@users.noreply.github.com>
2025-11-23 23:38:01 +0530

    #

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,12 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+// If DATABASE_URL has '?sslmode=require', standard drivers need this config:
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+    ssl: {
+        rejectUnauthorized: false // Required for Neon/Render connection
+    }
 });
 
 module.exports = pool;
